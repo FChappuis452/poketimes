ce77579d06bbd7df9aba7f1c7727b1709f2ab465
Frederic Chappuis <fchappuis452@gmail.com>
2020-05-05 00:39:44 -0400

    Lesson 25 - Adding navigation to site

---
+++ poketimes/src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className="container">
+      <h4 className="center">Contact</h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non similique, odio officia totam consectetur natus libero! Fugiat nemo dicta eius repellendus suscipit placeat, eligendi expedita, explicabo aliquam corporis illo earum.</p>
+    </div>
+  )
+}
+
+export default Contact
---
+++ poketimes/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="container">
+      <h4 className="center">Home</h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non similique, odio officia totam consectetur natus libero! Fugiat nemo dicta eius repellendus suscipit placeat, eligendi expedita, explicabo aliquam corporis illo earum.</p>
+    </div>
+  )
+}
+
+export default Home
